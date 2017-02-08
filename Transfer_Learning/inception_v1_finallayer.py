25d3701231155c964e6b3fa8029f2acab6457abb
PrakashR <vanapaliprakash@gmail.com>
2017-02-08 18:53:49 +0530

    rectified persistent problem
    previously tf.constant is leading to increased memory usage by adding all the images to the graph. so i removed it and a placeholder.

--- Transfer_Learning/inception_v1_finallayer.py
+++ Transfer_Learning/inception_v1_finallayer.py
@@ -1,31 +1,48 @@
 import tensorflow as tf
 slim = tf.contrib.slim
 
-from models import inception
 from datasets import dataset_utils
 
 from nets import inception
 from preprocessing import inception_preprocessing
 import cv2, glob, os, random, itertools
 import numpy as np
-import pandas as pd
 from tqdm import tqdm
 
+from ise.indexer.featureindexer import FeatureIndexer
+
+
+
+
 tf.app.flags.DEFINE_string(
-    'train_data', '/Users/Satish/Downloads/kaggle-fish/data/*',
+    'train_data', '/home/prakash/kaggle-fish/fish/data/*',
     'Directory where the image files are present.')
 
 tf.app.flags.DEFINE_string(
-    'checkpoint_dir', '/Users/Satish/Downloads/tensorflow-2017/checkpoints/inception_v1.ckpt',
+    'checkpoint_dir', '/home/prakash/Downloads/pre-trained/checkpoints/inception_v1.ckpt',
     'location of the checkpoint files')
 
 tf.app.flags.DEFINE_string(
     'tensor_name', 'InceptionV1/Logits/MaxPool_0a_7x7/AvgPool:0',
     'The name of the tensor which you want to access')
 
+tf.app.flags.DEFINE_string(
+'features_db', "features.hdf5",
+"the name of features file which will save the data")
+
+tf.app.flags.DEFINE_integer("approx_images", 3700, "The Approximate number of Images in the data")
+
+tf.app.flags.DEFINE_integer("max_buffer_size", 100, "The maximum buffer size to save the data")
+
+
 FLAGS = tf.app.flags.FLAGS
 
+
+
 image_size = inception.inception_v1.default_image_size
+x = tf.placeholder(tf.float32,[None,None,3])
+processed_image = inception_preprocessing.preprocess_image(x, image_size, image_size, is_training=False)
+processed_images  = tf.expand_dims(processed_image, 0)
 X = tf.placeholder(tf.float32, [None, image_size, image_size, 3])
 
 filelist = glob.glob(FLAGS.train_data)
@@ -47,23 +64,27 @@ with tf.Graph().as_default():
 init = tf.initialize_all_variables()
 restorer = tf.train.Saver()
 
+fi = FeatureIndexer(FLAGS.features_db,estNumImages=FLAGS.approx_images, maxBufferSize = FLAGS.max_buffer_size)
+print("[INFO] the database has been created")
+
+
 with tf.Session() as sess:
     sess.run(init)
     restorer.restore(sess, FLAGS.checkpoint_dir)
     p3 = sess.graph.get_tensor_by_name(FLAGS.tensor_name)
-    X_pool3 = []
+    #X_pool3 = []
     print("[INFO] feature collection has begun")
     for i in tqdm(range(len(filelist))):
         img= cv2.imread(filelist[i])
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = tf.constant(img, tf.float32)
-        processed_image = inception_preprocessing.preprocess_image(img, image_size, image_size, is_training=False)
-        processed_images  = tf.expand_dims(processed_image, 0)
-        image = sess.run(processed_images)
-        features = sess.run(p3,{X:image})
-        X_pool3.append(features)
+        m = sess.run(processed_images,feed_dict={x:np.float32(img)})
+        features = sess.run(p3,{X:m})
+        fi.add(filelist[i].rsplit("/")[-2],features.reshape([1,features.shape[3]]))
+
+
+fi.finish()
 
 print("[INFO] All the features have been collected . Saving it to a dataframe now")
-tags = [filelist[i].rsplit("/")[-2] for i in range(len(filelist))]
-images = np.vstack([np.reshape(X_pool3[i],-1) for i in range(len(X_pool3))])
-numpy.savetxt("fish.csv", np.c_[images, tags], delimiter=",")
+# tags = [filelist[i].rsplit("/")[-2] for i in range(len(filelist))]
+# images = np.vstack([np.reshape(X_pool3[i],-1) for i in range(len(X_pool3))])
+# numpy.savetxt("fish.csv", np.c_[images, tags], delimiter=",")
